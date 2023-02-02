e77524dbc5f7a984f9584fa27e3fcc57e44c2337
muzi <zim29144@gmail.com>
2023-02-02 19:18:28 +0800

    农场自动化添加代理

--- jd_farm_automation.js
+++ jd_farm_automation.js
@@ -1,5 +1,7 @@
 //20 5,12,21 * * * m_jd_farm_automation.js
 //问题反馈:https://t.me/Wall_E_Channel
+require('global-agent/bootstrap');
+global.GLOBAL_AGENT.HTTP_PROXY="http://192.168.3.3:7080";
 const {Env} = require('./function/magic');
 const $ = new Env('M农场自动化');
 let level = process.env.M_JD_FARM_LEVEL ? process.env.M_JD_FARM_LEVEL * 1 : 2
